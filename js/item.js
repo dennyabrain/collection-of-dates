f9248fb2a78cfd8a6019ba2a07a935d8f16b35e8
dennyabrain <denny.george90@gmail.com>
2017-01-04 01:24:54 -0500

    added twemoji. dynamic list working with shuffled items

---
+++ js/item.js
@@ -0,0 +1,62 @@
+/**
+ * Created by abrain on 1/3/17.
+ */
+
+var item = [
+    {
+        date:'December 26, 2016',
+        post:'I feel slightly bothered by the fact that you did not wish me on Christmas.'
+    },
+    {
+        date:'December 29, 2016',
+        post:'I begin thinking that you never really were a Christmas wisher, anyway. So maybe I should not be that upset.'
+    },
+    {
+        date:'January 1, 2017',
+        post:'I realize that I forgot to wish you on your birthday and so maybe I should not be upset AT ALL!'
+    },
+    {
+        date:'December 17, 2016',
+        post:'I go to an art exhibition on digital surveillance and see some 3D-like postcards. I feel that I might be wrong and I am probably wrong but they look like something that a strange being like you may cherish. You probably would just go meh and toss them amongst a pile of nothings. Or possibly you would totally go berserk about how cool they are and send it to loved ones. There is a small chance that this is possibly one of the best things I could give you. Since that is a rare occasion I am willing to tell the convinience seeking side of my brain to lay low and make plans of posting these to you.'
+    },
+    {
+        date:'July 2, 2013',
+        post:'You tell me you like postcards. I might be making this up or totally misremembering this 😜'
+    },
+    {
+        date:'August 17, 2015',
+        post:'You post a letter from Berkley to me hence revealing your address.'
+    },
+    {
+        date:'November 10, 2014',
+        post:'I think for the first time after being in the US that maybe I should send you a gift, but the logistics of international postage seemed like the valid excuse (back then) for me to not send you anything.'
+    },
+    {
+        date:'October 9, 2015',
+        post:'I decided I should definitely send you a present but what could I send that is not generic enough to feel meaningless and also not meaningful enough to feel like a subtle sublimal message 😜'
+    },
+    {
+        date:'December 27, 2016',
+        post:'I watch a really good movie La La Land and think of you. I wanted to recommend you to watch it but am too taken over by my ego because of the fact that you did not wish me on Christmas 😜'
+    },
+    {
+        date:'December 6, 2016',
+        post:'You post your images of goodle timelapse on your blog that makes a light go off in my dumb head. Even though you are a nerd of many things, and since I do not share a lot of those passions with you, it is hard to place a finger on what physical object you might like. But is it possible that a LOTR reading, Ursula Le Guin loving being might be into Maps? 😲'
+    },
+    {
+        date:'December 17, 2016',
+        post:'I am mildly dissapointed that these postcards are free. Because they feel like they should cost money. They look like something of value must be exchanged in order to acquire them. I prepare myself to to spend around 20 dollars for two postcard. I feel that if you like them, it would be nice for you to keep one for yourself and send the other one to a loved one. Since they were free I picked up all unique copies and also some not so good postcards. I am sure you would want to send postcards to your not-that-much-loved-ones too 😜'
+    },
+    {
+        date:'January 2, 2017',
+        post:'I decide to post these lovely postcards to you in an envelope because I am too worried that if I post them actually as postcards they will get stolen coz they look so good. I had to change plans because the post office was closed due to them observing an extended new year.'
+    },
+    {
+        date:'January 2, 2017',
+        post:'This was not planned at all but you happen to ping me and we talk on phone after so long. Throughout the call I am worried that your birthday will come up. When it does, I literally choke myself to prevent me from saying that \'Actually I was planning a surprise for you\'. I believe that it remains my best display of growth and character so far 😜'
+    },
+    {
+        date:'January 1, 2017',
+        post:'I finally figured out what the letter accompanying the postcards should be like.'
+    }
+];
--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,41 @@
-$(document).ready(function(){
-    console.log('ready');
-    twemoji.size = '16x16';
-    var t = twemoji.parse(document.body);
+'use strict';
 
-    console.log(t);
-})
+let populateItem = function(array, index, listNode){
+    let heading = document.createElement('h2');
+    let post = document.createElement('p');
+    heading.textContent=array[index].date;
+    post.textContent=array[index].post;
+
+    console.log(twemoji.parse(array[index].post));
+
+    //console.log(listNode);
+    listNode.appendChild(heading);
+    listNode.appendChild(post);
+}
+
+$(document).ready(function(){
+
+    //console.log(item.length);
+    let tempItem = item.slice(3, -1);
+
+    //console.log(item);
+    //console.log(tempItem);
+
+    let node=$('#list')[0];
+
+    for(let i=0;i<3;i++){
+        populateItem(item, i, node);
+    }
+
+    for(let i=3;i<item.length-2;i++){
+        let dice = Math.floor(Math.random()*tempItem.length);
+        populateItem(tempItem, dice, node);
+        tempItem.splice(dice, 1);
+    }
+
+    populateItem(item, item.length-1, node);
+
+    let itemList=$('#list')[0];
+    //console.log(document);
+    twemoji.parse(document);
+});
